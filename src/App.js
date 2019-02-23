ce6cebf6284757143bc54379900790b48e715640
marty <martymagaan@gmail.com>
2019-02-23 16:32:24 -0500

    refactor: move example quest array to variable

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import QuestList from './components/QuestList';
 
 function App() {
-  const [quests, setQuests] = useState([{name: 'Quest 1'}, {name: 'Quest 2'}]);
+  const exampleQuests = [
+    {name: 'Quest 1'},
+    {name: 'Quest 2'},
+    {name: 'Quest 3'}
+  ];
+
+  const [quests, setQuests] = useState(exampleQuests);
 
   return (
     <div className="App">
