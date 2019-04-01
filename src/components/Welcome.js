1f89e19c6e3f2b10ed4dc7ff7f85e7f17e9b383c
marty <martymagaan@gmail.com>
2019-03-31 23:33:25 -0400

    feat: add sign up form

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,6 +7,14 @@ function Welcome () {
       IRL Quest is an RPG-like quest menu for real life.
       Your quests will be saved locally on your browser.<br />
       If you want to save your quests across devices, sign up below:
+      <form>
+        <label>
+          Email<br />
+          <input type="text" name="email" placeholder="Enter email address" /><br />
+          <input type="text" name="confirm-email" placeholder="Confirm email address" />
+        </label><br />
+        <input type="submit" value="Sign Up" />
+      </form>
     </div>
   );
 }
